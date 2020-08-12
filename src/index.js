7c03a32387e78b3413e3cff946799968c3e3154c
mariosknl <mariosknl@outlook.com>
2020-08-12 10:45:48 +0300

    Store created

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 import App from './App';
+import rootReducer from './reducers/index';
+
+const store = createStore(state);
 
 ReactDOM.render(
   <React.StrictMode>
