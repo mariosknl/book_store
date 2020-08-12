d626ff569b34191753de8d65014e46e507e19978
Expedito Andrade <expeditojazz@gmail.com>
2020-08-12 10:39:15 -0300

    fix linter errors

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 const CategoryFilterSelect = styled.select`
