6a0b9759b7e9c3683758374e7e2c71015215395d
Expedito Andrade <expeditojazz@gmail.com>
2020-08-12 06:19:36 -0300

    add styled component for book form

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
+const StyledBookForm = styled.form`
+  background: yellow;
+  color: green;
+`;
 export default function BooksForm() {
+  const [formInputs, setForminputs] = useState({
+    title: '',
+    category: ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'],
+  });
   return (
-    <div>
+    <StyledBookForm>
       Hello from the books form
-    </div>
+    </StyledBookForm>
   );
 }
