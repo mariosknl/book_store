be09a968737db774489eb55ecfc4162367ad41e9
mariosknl <mariosknl@outlook.com>
2020-08-12 17:16:35 +0300

    navbar creation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,32 @@ const StyledNavbar = styled.nav`
   align-items: center;
   justify-content: space-between;
   background: red;
+  font-family: 'Montserrat', sans-serif;
+
+  ul {
+    display: flex;
+    align-items: center;
+    list-style: none;
+    li {
+      margin-left: 1rem;
+      color: #F4F5F9;
+      font-size: 13px;
+    }
+  }
+    i {
+      font-size: 20px;
+      padding: 0.5rem;
+      margin-right: 1rem;
+      border: 2px solid #000;
+      border-radius 50%;
+    }
 `;
 
 export default function Navbar() {
   return (
     <StyledNavbar>
       <ul>
-        <li>Bookstore CMS</li>
+        <li><h1>Bookstore CMS</h1></li>
         <li>Books</li>
         <li>Categories</li>
       </ul>
