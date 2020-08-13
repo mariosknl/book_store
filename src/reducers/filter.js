f5c4d321ca1a5e1711c1901de481b561ffeb96a4
mariosknl <mariosknl@outlook.com>
2020-08-13 16:44:59 +0300

    Fixed the requested issues

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,4 +1,6 @@
-export default (state = {}, action) => {
+const defaultState = { current: 'All', categories: ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'] };
+
+export default (state = defaultState, action) => {
   switch (action.type) {
     case ('CHANGE_FILTER'):
       return ({
